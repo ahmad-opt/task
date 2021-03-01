b9cbae1ef733c0748b1f50d1ca0eecb3fb19c68a
ahmad-opt <ahmad.sulieman@optimumpartners.co>
2021-03-01 16:41:12 +0200

    first commit

---
+++ config/db.php
@@ -0,0 +1,30 @@
+<?php
+class Database
+{
+    // database credentials
+    private $host = "localhost";
+    private $db_name = "task";
+    private $username = "ahmad";
+    private $password = "Qwertyuiop1@";
+    public $conn;
+
+    // get the database connection
+    public function getConnection()
+    {
+        $this->conn = null;
+        try
+        {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }
+        catch(PDOException $exception)
+        {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+    function __destruct() {
+      $this->conn = null;
+    }
+}
